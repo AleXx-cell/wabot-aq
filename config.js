70d315d19778419f6964bde9057b6f7e8b219199
AleXx-cell <81187199+AleXx-cell@users.noreply.github.com>
2021-03-23 05:26:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
-global.owner = ['6281515860089'] // Put your number here
+global.owner = ['18183333333'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
-  nrtm: 'https://nurutomo.herokuapp.com',
-  xteam: 'https://api.xteam.xyz'
+  nrtm: 'https://invalidentry¡!.li',
+  xteam: 'https://invalidentry.li'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b'
+  'https://invalidentry.li': 'd90a9e986e18778b'
 }
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'AleXx'
 
 
 
